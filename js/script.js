af79c62e1c8e8f9c06d7d03be884fdb8e5268860
Joseph Monreal <joemonreal23@icloud.com>
2019-07-14 22:37:50 -0700

    Working on (3) T-Shirt Section

--- js/script.js
+++ js/script.js
@@ -2,110 +2,85 @@
 Treehouse Techdegree: FSJS Project 3 - Interactive Form
 ******************************************/
 
-/* 
-    1. Set focus on the first text field
-        • When the page first loads, the first text field should be in focus by default.
-        
-*/
-$('#name').focus();
-
-/* 
-    2. "Job Role" section
-        • Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
-        • Give the field an id of “other-title,” and add the placeholder text of "Your Job Role".
-    Note: You'll need to add the "Other" job role input directly into the HTML and hide it initially with JS in order to get this feature to work when JS is disabled, which is a requirement below.
-
-*/
-let element = $('#title').children();
-element.each(function(index, ele){
+$(document).ready(function () {
     /*
-    if(index == 5) {
-        index.on("click", function() {
-            $("input").attr("id", "other-title");
-            $("input").text("Your Job Role");
-            //index.append()
-        });
-    }
+            (1) Put the first field in the `focus` state
+            
+            • Use jQuery to select the `Name` input element and place focus on it.
+                
+
     */
-    console.log(ele);
+    $('#name').focus();
+
+    /*
+            (2) Add an "Other" option to the Job Role section
+            
+            • In the `index.html` file, just below the `Job Role` select element, create a text input element, set its `name` attribute to "job_role_other", set its `placeholder` attribute to "Your Job Role", and give it an "id" attribute of "other-title"
+            so you can easily target this element in your JS file. 
+            
+            • In your JavaScript file, target the 'Other' input field, and hide it initially, so that it will display if JavaScript is disabled, but be hidden initially with JS.
+            
+    */
+
+    $('#other-title').hide();
+    let lastValue = $('#title option:last-child').val();
+    $('#title').change(function () {
+        if($(this).val() == 'other') {
+            $('#other-title').show('slow');
+        } else {
+            $('#other-title').hide('slow');
+        }
+    });
 });
-//console.log(element);
 
-/* 
-    3. ”T-Shirt Info” section
-        • For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
-        • If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
-        • If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
-        • When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
-*/
-
-/* 
-    4. ”Register for Activities” section
-        • Some events are at the same day and time as others. If the user selects a workshop, don't allow selection of a workshop at the same day and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-        • When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
-        • As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.
-*/
-
-/* 
-    5. "Payment Info" section
-        • Display payment sections based on the payment option chosen in the select menu.
-        • The "Credit Card" payment option should be selected by default. Display the #credit-card div, and hide the "PayPal" and "Bitcoin" information. Payment option in the select menu should match the payment option displayed on the page.
-        • When a user selects the "PayPal" payment option, the PayPal information should display, and the credit card and “Bitcoin” information should be hidden.
-        • When a user selects the "Bitcoin" payment option, the Bitcoin information should display, and the credit card and “PayPal” information should be hidden.
-        
-    Note: The user should not be able to select the "Select Payment Method" option from the payment select menu, because the user should not be able to submit the form without a chosen payment option.
-*/
-
-/* 
-    6. Form validation
-        • If any of the following validation errors exist, prevent the user from submitting the form:
-            • Name field can't be blank.
-            • Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
-            • User must select at least one checkbox under the "Register for Activities" section of the form.
-            • If the selected payment option is "Credit Card," make sure the user has supplied a Credit Card number, a Zip Code, and a 3 number CVV value before the form can be submitted.
-                • Credit Card field should only accept a number between 13 and 16 digits.
-                • The Zip Code field should accept a 5-digit number.
-                • The CVV should only accept a number that is exactly 3 digits long.
-    Note: Don't rely on the built in HTML5 validation by adding the required attribute to your DOM elements. You need to actually create your own custom validation checks and error messages.
-    
-    Note: Avoid using snippets or plugins for this project. To get the most out of the experience, you should be writing all of your own code for your own custom validation.
-    
-    Note: Make sure your validation is only validating Credit Card info if Credit Card is the selected payment method.
-*/
-
-/* 
-    7. Form validation messages
-        • Provide some kind of indication when there’s a validation error. The field’s borders could turn red, for example, or even better for the user would be if a red text message appeared near the field.
-        • The following fields should have some obvious form of an error indication:
-            • Name field
-            • Email field
-            • Register for Activities checkboxes (at least one must be selected)
-            • Credit Card number (Only if Credit Card payment method is selected)
-            • The Zip Code field should accept a 5-digit number.
-            • The CVV should only accept a number that is exactly 3 digits long.
-    Note: Error messages or indications should not be visible by default. They should only show upon submission, or after some user interaction.
-    
-    Note: Avoid use alerts for your validation messages.
-*/
-
-/* 
-    8. Form works without JavaScript - Progressive Enhancement
-        • The user should still have access to all form fields and payment information if JS isn't working for whatever reason. For example, when the JS is removed from the project:
-            • The “Other” text field under the "Job Role" section should be visible
-            • All information for Bitcoin, PayPal or Credit Card payments should be visible.
-*/
-
-/* 
-    9. Extra Credit
-    
-        T-Shirt Section
-            • Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
+   /*
+            (3) T-Shirt Section
             
-        Conditional Error Message
-            • Program at least one of your error messages so that more information is provided depending on the error. For example, if the user hasn’t entered a credit card number and the field is completely blank, the error message reads “Please enter a credit card number.” If the field isn’t empty but contains only 10 numbers, the error message reads “Please enter a number that is between 13 and 16 digits long.”
-        Real-time Error Messages
-            • Program your form so that it provides a real-time validation error message for at least one text input field. Rather than providing an error message on submit, your form should check for errors and display messages as the user begins typing inside a text field. For example, if the user enters an invalid email address, the error appears as the user begins to type, and disappears as soon as the user has entered a complete and correctly formatted email address. You must accomplish this will your own JavaScript code. Do not rely on HTML5's built-in email validation.
+            • The goal for the t-shirt section is to filter the available "Color" options by the selected theme in the "Design" field. Doing this ensures that the user cannot select an invalid combination of values for the "Design" and "Color" fields.
             
-        NOTE: If you implement the above exceeds requirements in your form, make sure you detail in your submission notes which input will have different error messages depending on the error, and which input will have "real time" validation messages, so your reviewer won't miss them by accident.
+            • When the form is intially loaded, we need to update the "Design" and "Color" field so that it's clear to the user that they need to select a theme before selecting a color.
+            
+                • Hide the "Select Theme" `option` element in the "Design" menu. 
+                • Update the “Color” field to read “Please select a T-shirt theme”.
+                • Hide the colors in the “Color” drop down menu.
+                
+            • Then, when one of the two themes is selected, only the appropriate colors should show in the "Color" drop down menu, and the "Color" field should update to the first available color. You'll use a `change` event listener on the "Design" menu `select` element to listen for changes. And inside the event listener, you'll use a conditional to determine what to hide, show and update.
+            
+                • If "js puns" is selected, hide the three "heart js" option element in the "Color" drop down menu, show the three "js puns" option elements, and update the "Color" field to the first available color.
+                
+                • If "heart js" is selected, hide the three "js puns" option elements in the "Color" drop down menu, show the three "heart js" option elements, and update the "Color" field to the first available color.
+
+            
+    */
+$("#design option").eq(0).hide();
+$("#color").prepend($('<option>Please select a T-shirt theme</option>'));
+$("#color option").eq(0).attr('data-brackets-id', '449');
+$("#color option").eq(0).attr('style', 'display: none;');
+$("#color option").eq(0).attr('selected', 'selected');
+$("#color option:not(:eq(0))").hide();
+
+/*
+$("#color option").each(function(index, element) {
+    console.log(element);
+});
 */
 
+$("#design").change(function(){
+    console.log($(this));
+    if($(this).val() === 'js puns'){
+        $("#color option").eq(0).attr('selected');
+        $("#color option:lt(4)").show();
+        $("#color option").eq(4).attr('selected');
+        $("#color option").eq(1).attr('selected', 'selected').show();
+        $("#color option:gt(3)").hide();
+        $("#color option").eq(0).hide();
+    }
+    if($(this).val() === 'heart js'){
+        $("#color option").eq(0).attr('selected');
+        $("#color option").eq(1).attr('selected');
+        $("#color option").eq(4).attr('selected', 'selected').show();
+        $("#color option:gt(3)").show();
+        $("#color option").eq(0).hide();
+        $("#color option:lt(4)").hide();
+    }
+});
